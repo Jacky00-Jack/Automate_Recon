59f482bc238ce42c82d74ee4c2d06b2282f4f477
Jacky00-Jack <105658550+Jacky00-Jack@users.noreply.github.com>
2023-04-25 16:18:09 +0100

    Add files via upload

---
+++ auto_sorting.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import itertools as iter
+import numpy as np
+# find a way to connect it to Excel sheets
+# Use panda to import Excel, but how to make it dynamic (User friendly)
+# Use the zip function to tie the item ID to the formula used
+df = pd.read_csv(r"C:\Users\ADCCYW9\Downloads\Real Test 1.csv")
+# Use this expression to remove ','
+df['Amount'] = df['Amount'].str.replace(',', '')
+df['Amount'] = df['Amount'].astype(float)
+
+# String cant convert to float when there are special characters
+# Filter the Type and then apply function on the Corresponding Value
+df['Amount'] = np.where(df['Item Type'].str.contains('S CR'),-1*df['Amount'], np.where(df['Item Type'].str.contains('L DR'),-1*df['Amount'],df['Amount']))
+print(df, '\n', '\n')
+
+# the 2 in range ignores singles
+combos = []
+for i in range(2, df['Amount'].count() + 1):
+    combos.extend(list(iter.combinations(df['Amount'], i)))
+
+# abs() to make it ascending in regard to 0
+combo_sums = []
+for combo in combos:
+    combo_sum = abs(sum(combo))
+    combo_sums.append(combo_sum)
+
+combo_sums_sorted, combos_sorted = zip(*sorted(zip(combo_sums, combos)))
+# Change len(combo_sums_sorted) to an x to print top x result
+for i in range(10):
+    combination = pd.DataFrame(combos_sorted[i])
+    for j in range(combination[0].count()):
+        z = df[df['Amount'] == combination.iat[j, 0]]
+        for item_id in z['Item ID']:
+            print(z['Amount', 'Item ID'])
+    print('Outstanding: ', combo_sums_sorted[i])
+    print('\n')
+exit()
+for i in range(10):
+    combination = combos_sorted[i]
+    for j in range(len(combination)):
+        z = df[df['Amount'] == combination[j]]
+    print(z)
+    print('Outstanding: ', combo_sums_sorted[i])
+    print('\n')
+exit()
+
+
+combination['Item ID'] = df['Item ID'].str.contains(combination.iat[j, 0])
+print('Outstanding: ', combo_sums_sorted[i], '\n', 'Combos: ', '\n', combination, '\n')
+exit()
+# Same value date
+# CSV, PANDA
+# Feed data into this from TLM
+# Know python's potential
+# Understand TLM completely
+# Write off
+combination['Item ID'] = item[combos_sorted[i]]
+print((df['Item ID']).to_string(index = False))
